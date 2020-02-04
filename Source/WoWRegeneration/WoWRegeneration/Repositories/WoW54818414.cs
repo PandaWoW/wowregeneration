02d0d425137014e7cd776a86356b8829936c3f37
DeadMouse <max180297@ya.ru>
2020-02-05 01:03:57 +0200

    Added 18414 support.

---
+++ Source/WoWRegeneration/WoWRegeneration/Repositories/WoW54818414.cs
@@ -0,0 +1,15 @@
+﻿namespace WoWRegeneration.Repositories
+{
+    public class WoW54818414 : WoWRepository
+    {
+        public override string GetBaseUrl()
+        {
+            return "http://dist.blizzard.com.edgesuite.net/wow-pod-retail/NA/15890.direct/";
+        }
+
+        public override string GetMFilName()
+        {
+            return "wow-18414-447E3E618F731CCBF4F7D2C4E56C5644.mfil";
+        }
+    }
+}
